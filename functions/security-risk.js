a9df0c7f4afd3775debeeddf969ddb98c24f7266
netcorelabs <32109157+netcorelabs@users.noreply.github.com>
2026-02-17 16:03:03 -0500

    Update security-risk.js

--- functions/security-risk.js
+++ functions/security-risk.js
@@ -1,7 +1,7 @@
 exports.handler = async function(event) {
 
   const headers = {
-    "Access-Control-Allow-Origin": "*",
+   "Access-Control-Allow-Origin": "https://www.homesecurecalculator.com"
     "Access-Control-Allow-Headers": "Content-Type",
     "Access-Control-Allow-Methods": "GET, OPTIONS"
   };
