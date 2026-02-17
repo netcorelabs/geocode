825c2d9547d34c98256ed1e75f4f74c8915f7583
netcorelabs <32109157+netcorelabs@users.noreply.github.com>
2026-02-17 17:18:37 -0500

    Update get-google-key.js

--- netlify/functions/get-google-key.js
+++ netlify/functions/get-google-key.js
@@ -1,1 +1,12 @@
 
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      key: process.env.GOOGLE_MAPS_KEY
+    })
+  };
+};
