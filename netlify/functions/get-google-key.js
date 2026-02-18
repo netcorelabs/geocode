23b3c014f726062677a5a94139682bff42bda6cb
netcorelabs <32109157+netcorelabs@users.noreply.github.com>
2026-02-17 19:13:59 -0500

    Update get-google-key.js

--- netlify/functions/get-google-key.js
+++ netlify/functions/get-google-key.js
@@ -1,12 +1,26 @@
+exports.handler = async function () {
 
-exports.handler = async () => {
+  if (!process.env.GOOGLE_API_KEY) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
+      body: JSON.stringify({
+        error: "GOOGLE_API_KEY not configured"
+      })
+    };
+  }
+
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type"
     },
     body: JSON.stringify({
-      key: process.env.GOOGLE_MAPS_KEY
+      key: process.env.GOOGLE_API_KEY
     })
   };
 };
