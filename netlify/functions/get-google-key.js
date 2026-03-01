17698bbf70790d1ff51af701080c43ba6c000232
netcorelabs <32109157+netcorelabs@users.noreply.github.com>
2026-03-01 00:34:34 -0500

    Update get-google-key.js

--- netlify/functions/get-google-key.js
+++ netlify/functions/get-google-key.js
@@ -1,18 +1,11 @@
-export async function handler(event, context) {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  };
-
-  if (event.httpMethod === "OPTIONS") {
-    return { statusCode: 204, headers };
-  }
-
-  // Return your Google API key securely
+exports.handler = async () => {
   return {
     statusCode: 200,
-    headers,
-    body: JSON.stringify({ key: process.env.GOOGLE_MAPS_KEY }),
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      key: process.env.GOOGLE_MAPS_KEY
+    })
   };
-}
+};
